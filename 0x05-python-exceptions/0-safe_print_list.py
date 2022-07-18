7c879f9d43be79e6f938a094794bf1cc7f28a668
pouchkine <pouchkinecamerdouala@gmail.com>
2022-07-18 06:52:13 -0700

    fix space error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-            count + = 1
+            count = count + 1
         print("")
     except IndexError:
         print('')
