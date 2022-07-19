6d3d286fc68a6f6dfec208e06189e2b7029d506f
pouchkine <pouchkinecamerdouala@gmail.com>
2022-07-19 12:08:06 -0700

    long docstrings

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,9 @@
 class Square():
     """ The Square class doesn't have any attribute
     it is an empty class
+    but we requred to write many line
+    as docsting to explain things that can be explainred in one
+    sentence.
+    each objet of this class doent have nothing at all.
     """
     pass
