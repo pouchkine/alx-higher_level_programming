0d48d0ca65f192b2889884c61716a33dd4a38724
pouchkine <pouchkinecamerdouala@gmail.com>
2022-07-19 12:15:52 -0700

    more long docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,41 @@
 #!/usr/bin/python3
 class Square():
     """ The Square class doesn't have any attribute
-    it is an empty class
-    but we requred to write many line
-    as docsting to explain things that can be explainred in one
-    sentence.
-    each objet of this class doent have nothing at all.
+it is an empty class
+but we requred to write many line
+as docsting to explain things that can be explainred in one
+sentence.
+each objet of this class doent have nothing at all.
+The __init__ method may be documented in either the class level
+
+    docstring, or as a docstring on the __init__ method itself.
+
+
+
+    Either form is acceptable, but the two should not be mixed. Choose one
+
+    convention to document the __init__ method and be consistent with it.
+
+
+
+    Note:
+
+        Do not include the `self` parameter in the ``Args`` section.
+
+
+
+    Args:
+
+        msg (str): Human readable string describing the exception.
+
+        code (:obj:`int`, optional): Error code.
+
+
+
+    Attributes:
+
+        msg (str): Human readable string describing the exception.
+
+        code (int): Exception error code.
     """
     pass
