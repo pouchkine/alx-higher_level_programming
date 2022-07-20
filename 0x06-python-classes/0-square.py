eaf7c7431e88373dfa7e107fafb99d3ee2d1c4bc
pouchkine <pouchkinecamerdouala@gmail.com>
2022-07-19 19:20:20 -0700

    adding space beetween the first and the second line in the docstring

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square():
     """ The Square class doesn't have any attribute
+
 it is an empty class
 but we requred to write many line
 as docsting to explain things that can be explainred in one
